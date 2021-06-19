4d3e2b9720f66cc5b8d996914bd9dd0c917a55d0
MayurThakar <mayur.prohgramer@gmail.com>
2021-06-19 09:56:18 +0530

    first develop

--- project/urls.py
+++ project/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 from app.views import index
 
 urlpatterns = [
     path('', index, name='index'),
     path('admin/', admin.site.urls),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
